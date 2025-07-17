6aa716188cf1ae9bcd69372cd7d9295b8f249475
beulrakbi <iamjyh99@gmail.com>
2025-07-17 12:14:54 +0900

    알고리즘

--- src/maxMin/Min3.java
+++
@@ -1,7 +0,0 @@
-package maxMin;
-
-public class Min3 {
-    public static void main(String[] args) {
-        System.out.println("min:" + SelectMethodMin1.min(2, 1, 3));
-    }
-}
--- src/maxMin/Min4.java
+++
@@ -1,7 +0,0 @@
-package maxMin;
-
-public class Min4 {
-    public static void main(String[] args) {
-        System.out.println("min:" + SelectMethodMin2.min(1, 2, 3, 0));
-    }
-}
